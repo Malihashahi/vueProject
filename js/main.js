28ab5b35551a0352c7fcd1b1961485c9ca20077f
Malihashahi <maliha.shahi.03@gmail.com>
2025-02-09 21:10:26 +0330

    worh with v-bind , v-text,v-html

--- js/main.js
+++ js/main.js
@@ -2,15 +2,13 @@ Vue.createApp(
   {  
      data(){
         return{
-           title : 'webpage',
-           boolean : true,
-           obj: {
-             name :'maliha shahi'
-           },
-           array :['mliha ','bahar'],
-           fun :() =>{
-             return 'start vue.js'
-           }
+          src :'https://picsum.photos/200/300/?blur=2',
+          alt :'test text',
+          href: 'https://vuejs.org/guide/introduction.html',
+          text :'this v-text',
+          html : '<strong>google.com</strong>'
+
+          
         }
      }
    }
