28ab63d9f8e81e89414a89961e250ce7d207123c
Malihashahi <maliha.shahi.03@gmail.com>
2025-02-09 22:01:02 +0330

    worh with v-if , v-else

--- js/main.js
+++ js/main.js
@@ -2,12 +2,9 @@ Vue.createApp(
   {  
      data(){
         return{
-          src :'https://picsum.photos/200/300/?blur=2',
-          alt :'test text',
-          href: 'https://vuejs.org/guide/introduction.html',
-          text :'this v-text',
-          html : '<strong>google.com</strong>'
-
+     
+        number : 3,
+        isstatus : true
           
         }
      }
