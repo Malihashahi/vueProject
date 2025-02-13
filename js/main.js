1a167e91f8e050b438cf34ecaf1037816e0c8cfd
Malihashahi <maliha.shahi.03@gmail.com>
2025-02-13 23:43:07 +0330

    work with refs tempelet

--- js/main.js
+++ js/main.js
@@ -2,30 +2,14 @@ Vue.createApp(
   {  
      data(){
         return{
-         name :'',
-         active: false
+        
         }
      },
-     watch:{
-       active(value){
-         console.log(value)
-       }
-     },
-     computed:{
-          isActive(){
-              console.log('uppercase !!')
-              return this.active ? 'Active' : 'not Active'
-      }
-     },
+     
      methods:{
-       upperCase(){
-          console.log('uppercase !!1')
-           return this.name.toUpperCase()
-      },
-      toggleActive(){
-         this.active = !this.active
-      }
-      
+       button(){
+         console.log(this.$refs.title.style.color='red')
+       }
 
      }
     
