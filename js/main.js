9ab2e8013fff36499ea82f8504881bad3da13e54
Malihashahi <maliha.shahi.03@gmail.com>
2025-02-13 23:13:41 +0330

    work with watch and computed

--- js/main.js
+++ js/main.js
@@ -2,16 +2,32 @@ Vue.createApp(
   {  
      data(){
         return{
-         colors:{
-              redClass:'red',
-              blueClass: 'blue'
-         }
+         name :'',
+         active: false
         }
      },
-     methods:{
-      changeColor(){
-         
+     watch:{
+       active(value){
+         console.log(value)
+       }
+     },
+     computed:{
+          isActive(){
+              console.log('uppercase !!')
+              return this.active ? 'Active' : 'not Active'
       }
+     },
+     methods:{
+       upperCase(){
+          console.log('uppercase !!1')
+           return this.name.toUpperCase()
+      },
+      toggleActive(){
+         this.active = !this.active
+      }
+      
+
      }
+    
    }
 ).mount('#app');
