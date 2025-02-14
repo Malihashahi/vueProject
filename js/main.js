6df081e1c9a20c0d9c22d664f22cc5ec95ef946d
Malihashahi <maliha.shahi.03@gmail.com>
2025-02-14 23:23:24 +0330

    lifecycle hook

--- js/main.js
+++ js/main.js
@@ -2,15 +2,35 @@ Vue.createApp(
   {  
      data(){
         return{
-        
+         name :'nargis'
+
         }
      },
      
      methods:{
-       button(){
-         console.log(this.$refs.title.style.color='red')
-       }
+      updateName(){
+        this.name ='bahar'
+      }
 
+     },
+     beforeCreate(){
+        console.log('Running before create')
+     },
+     created(){
+      console.log('running created')
+     },
+     beforeMount(){
+      console.log('running before mount')
+     },
+     mounted(){
+      console.log('running mounted')
+     },
+     beforeUpdate(){
+      console.log('running before update')
+     },
+     updated(){
+      console.log('running  updated')
+    
      }
     
    }
