31b169c974c5b5b27625620edd92fd61195dc97c
Malihashahi <maliha.shahi.03@gmail.com>
2025-02-15 21:31:32 +0330

    work on form elemnt

--- js/main.js
+++ js/main.js
@@ -2,36 +2,19 @@ Vue.createApp(
   {  
      data(){
         return{
-         name :'nargis'
-
+          formData:{
+             name : '',
+              message : ''
+          }
         }
+        
      },
      
      methods:{
-      updateName(){
-        this.name ='bahar'
+      submitForm(){
+         console.log(this.formData)
       }
 
-     },
-     beforeCreate(){
-        console.log('Running before create')
-     },
-     created(){
-      console.log('running created')
-     },
-     beforeMount(){
-      console.log('running before mount')
-     },
-     mounted(){
-      console.log('running mounted')
-     },
-     beforeUpdate(){
-      console.log('running before update')
-     },
-     updated(){
-      console.log('running  updated')
     
-     }
-    
-   }
+   }}
 ).mount('#app');
